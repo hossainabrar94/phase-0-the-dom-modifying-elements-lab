fd919bd67cd368890e7eec97e57dc50ed42cefa0
Abrar <hossainabrar@aol.com>
2023-10-29 17:14:01 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+/*const main = document.getElementsByTagName("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent("Abrar is the champion"); */
+
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Abrar is the champion";
